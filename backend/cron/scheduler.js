6b9de26bed653911ebb9432cc5eee5cddac6de14
Louis Volant <volantlouis@gmail.com>
2025-03-21 18:40:00 +0100

    Fix import which prevented from sending the mail after scheduled check.

--- backend/cron/scheduler.js
+++ backend/cron/scheduler.js
@@ -1,12 +1,16 @@
 // cron/scheduler.js
 const { checkSkiPassStation } = require('../service/skipass-resort-call');
+const { sendMail } = require('../service/mailer-service');
 const express = require('express');
 const router = express.Router();
 
 router.get('/scheduler', async (req, res) => {
   console.log('Starting scheduled check...');
   const checkResults = await checkSkiPassStation();
+  console.log('Check results obtained:', checkResults);
   const mailResult = await sendMail(checkResults);
+  console.log('Mail sending completed:', mailResult);
+
   res.status(200).json({ message: 'Scheduled check completed', mailResult });
 });
 
