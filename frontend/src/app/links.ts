b7e71875e88d080a9aacb2c25d02c29811c19c20
Louis Volant <volantlouis@gmail.com>
2025-04-04 19:55:00 +0100

    Updated links

--- frontend/src/app/links.ts
+++ frontend/src/app/links.ts
@@ -11,8 +11,8 @@ export const internalLinks: LinkItem[] = [
 export const externalLinks: LinkItem[] = [
   { label: 'Personal Page', href: 'https://www.louisvolant.com' },
   { label: 'Password Keeper', href: 'https://www.securaised.net' },
-  { label: 'QR Code Tool', href: 'https://qr-code-tool.louisvolant.com' },
   { label: 'MP3 Tool', href: 'https://mp3-tool.louisvolant.com' },
-  { label: 'Random Text Generator', href: 'https://random-text-generator.louisvolant.com' },
+  { label: 'Rain Under The Cloud', href: 'https://rainunderthe.cloud' },
+  { label: 'Build My CV', href: 'https://buildmycv.net' },
   { label: 'My 20 years old blog', href: 'https://www.abricocotier.fr' },
 ];
